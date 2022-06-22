6262e1206cf8fd0d9592bbbbe89309cee74f855d
Lauren West <laurenwest12@gmail.com>
2022-06-22 11:16:27 -0400

    Updated to use last modified on

--- andromeda.js
+++ andromeda.js
@@ -3,13 +3,10 @@ const { url } = require('./config.js');
 const { updateProessedFlag } = require('./sql.js');
 
 const getAndromedaData = async (query, start) => {
-  // //Custom query example
-  // res = await axios.post(`${url}/search/query/${query}`, {
-  //   getafterdate: start,
-  // });
-
   // Get all development styles from Andromeda
-  const res = await axios.get(`${url}/bo/${query}`);
+  const res = await axios.post(`${url}/search/query/${query}`, {
+    getafterdate: start,
+  });
 
   // Create an array of only the relevant data for each development style
   const sourceStyleData = res.data.map(
@@ -38,12 +35,12 @@ const getAndromedaData = async (query, start) => {
   );
 
   /*
-	Return only styles where...
-			1. The source style is not blank
-			2. The source style does not match the style number of the development style
-			3. The carry forward flag is set to true
-	These are the ones where the carry forward flag needs to be updated
- */
+  	Return only styles where...
+  			1. The source style is not blank
+  			2. The source style does not match the style number of the development style
+  			3. The carry forward flag is set to true
+  	These are the ones where the carry forward flag needs to be updated
+   */
   return sourceStyleData.filter(
     ({ SourceStyle, Style, CarryForward }) =>
       SourceStyle !== '' && SourceStyle !== Style && CarryForward === 'Yes'
--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 const { type } = require('./config.js');
 const { andromedaAuthorization } = require('./authorization.js');
 const { sendErrorReport } = require('./functions/errorReporting.js');
-const { connectDb, submitAllQueries, getSQLServerData } = require('./sql');
+const {
+  connectDb,
+  submitAllQueries,
+  getSQLServerData,
+  getLastRunTime,
+} = require('./sql');
 const { getAndromedaData, updateAndromedaData } = require('./andromeda.js');
 
 const server = app.listen(6000, async () => {
@@ -14,20 +19,26 @@ const server = app.listen(6000, async () => {
     await andromedaAuthorization();
     await connectDb();
 
-    // 1. Get all styles created after last run time and filter to only styles that need to be updated
-    const data = await getAndromedaData('DevelopmentStyle');
+    // 1. Get the last time the program was ran
+    const lastRunTime = await getLastRunTime('SourceStyleImport', 'CreatedOn');
 
-    // 2. Insert into SourceStyleImport
+    // 2. Get all styles created after last run time and filter to only styles that need to be updated
+    const data = await getAndromedaData(
+      'ECHO-DevelopmentStyleSourceCreatedOn-1',
+      lastRunTime
+    );
+
+    // 3. Insert into SourceStyleImport
     const submitErrors = await submitAllQueries(data, 'SourceStyleImport');
     errors.push(submitErrors);
 
-    // 3. Get all styles from SourceStyleImport where AndromedaProcessed = 'No'. This is done separately to make sure we are updating any styles that could not get updated the previous run because someone was currently editing the style in Andromeda.
+    // 4. Get all styles from SourceStyleImport where AndromedaProcessed = 'No'. This is done separately to make sure we are updating any styles that could not get updated the previous run because someone was currently editing the style in Andromeda.
     const stylesToUpdate = await getSQLServerData(
       'SourceStyleImport',
       `WHERE AndromedaProcessed = 'No'`
     );
 
-    // 4. Update carry forward flag to false in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
+    // 5. Update carry forward flag to false in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
     const updateErrors = await updateAndromedaData(stylesToUpdate);
     errors.push(updateErrors);
   } catch (err) {
