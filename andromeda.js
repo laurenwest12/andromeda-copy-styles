c7fa07e72b9c275b67dd5cdd0a9769fbfa5c5ef9
Lauren West <laurenwest12@gmail.com>
2022-07-07 13:48:27 -0400

    Added code to also insert original season year

--- andromeda.js
+++ andromeda.js
@@ -10,7 +10,15 @@ const getAndromedaData = async (query, start) => {
 
   // Create an array of only the relevant data for each development style
   const sourceStyleData = res.data.map(
-    ({ id_developmentstyle, season, style, sourcestyle, cat33, createdon }) => {
+    ({
+      id_developmentstyle,
+      season,
+      style,
+      sourcestyle,
+      cat33,
+      cat24,
+      createdon,
+    }) => {
       let SourceSeason = '';
       let SourceStyle = '';
 
@@ -27,6 +35,7 @@ const getAndromedaData = async (query, start) => {
         SourceSeason,
         SourceStyle,
         CarryForward: cat33 ? 'Yes' : 'No',
+        OriginalSeasonYear: cat24,
         CreatedOn: createdon.substring(0, 19),
         AndromedaProcessed: 'No',
         AndromedaProcessedTime: '',
@@ -34,17 +43,20 @@ const getAndromedaData = async (query, start) => {
     }
   );
 
-  /*
-  	Return only styles where...
-  			1. The source style is not blank
-  			2. The source style does not match the style number of the development style
-  			3. The carry forward flag is set to true
-  	These are the ones where the carry forward flag needs to be updated
-   */
-  return sourceStyleData.filter(
-    ({ SourceStyle, Style, CarryForward }) =>
-      SourceStyle !== '' && SourceStyle !== Style && CarryForward === 'Yes'
-  );
+  // Return only styles where the source style is not blank
+  return sourceStyleData.filter(({ SourceStyle }) => SourceStyle !== '');
+
+  // /*
+  // 	Return only styles where...
+  // 			1. The source style is not blank
+  // 			2. The source style does not match the style number of the development style
+  // 			3. The carry forward flag is set to true
+  // 	These are the ones where the carry forward flag needs to be updated
+  //  */
+  // return sourceStyleData.filter(
+  //   ({ SourceStyle, Style, CarryForward }) =>
+  //     SourceStyle !== '' && SourceStyle !== Style && CarryForward === 'Yes'
+  // );
 };
 
 const updateAndromedaData = async (data) => {
--- index.js
+++ index.js
@@ -20,27 +20,28 @@ const server = app.listen(6000, async () => {
     await connectDb();
 
     // 1. Get the last time the program was ran
-    const lastRunTime = await getLastRunTime('SourceStyleImport', 'CreatedOn');
+    // const lastRunTime = await getLastRunTime('SourceStyleImport', 'CreatedOn');
+    const lastRunTime = '2020-06-01T12:21:26.000Z';
 
-    // 2. Get all styles created after last run time and filter to only styles that need to be updated
+    // 2. Get all styles that have a source style
     const data = await getAndromedaData(
       'ECHO-DevelopmentStyleSourceCreatedOn-1',
       lastRunTime
     );
 
     // 3. Insert into SourceStyleImport
-    const submitErrors = await submitAllQueries(data, 'SourceStyleImport');
+    const submitErrors = await submitAllQueries(data, 'SourceStyleImportNew');
     errors.push(submitErrors);
 
-    // 4. Get all styles from SourceStyleImport where AndromedaProcessed = 'No'. This is done separately to make sure we are updating any styles that could not get updated the previous run because someone was currently editing the style in Andromeda.
-    const stylesToUpdate = await getSQLServerData(
-      'SourceStyleImport',
-      `WHERE AndromedaProcessed = 'No'`
-    );
+    // // 4. Get all styles from SourceStyleImport where AndromedaProcessed = 'No'. This is done separately to make sure we are updating any styles that could not get updated the previous run because someone was currently editing the style in Andromeda.
+    // const stylesToUpdate = await getSQLServerData(
+    //   'SourceStyleImport',
+    //   `WHERE AndromedaProcessed = 'No'`
+    // );
 
-    // 5. Update carry forward flag to false in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
-    const updateErrors = await updateAndromedaData(stylesToUpdate);
-    errors.push(updateErrors);
+    // // 5. Update carry forward flag to false in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
+    // const updateErrors = await updateAndromedaData(stylesToUpdate);
+    // errors.push(updateErrors);
   } catch (err) {
     errors.push({
       type,
