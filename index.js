73376e132840002f94e2f78f9b648e89a0e7a23f
Lauren West <laurenwest12@gmail.com>
2022-07-07 16:57:54 -0400

    Uncommented everything and updated table back

--- index.js
+++ index.js
@@ -20,51 +20,49 @@ const server = app.listen(6000, async () => {
     await andromedaAuthorization();
     await connectDb();
 
-    // // 1. Get the last time the program was ran
-    // // const lastRunTime = await getLastRunTime('SourceStyleImport', 'CreatedOn');
-    // const lastRunTime = '2020-06-01T12:21:26.000Z';
+    // 1. Get the last time the program was ran
+    const lastRunTime = await getLastRunTime('SourceStyleImport', 'CreatedOn');
 
-    // // 2. Get all styles that have a source style
-    // const data = await getAndromedaData(
-    //   'ECHO-DevelopmentStyleSourceCreatedOn-1',
-    //   lastRunTime
-    // );
+    // 2. Get all styles that have a source style
+    const data = await getAndromedaData(
+      'ECHO-DevelopmentStyleSourceCreatedOn-1',
+      lastRunTime
+    );
 
-    // // 3. Insert into SourceStyleImport
-    // const submitErrors = await submitAllQueries(data, 'SourceStyleImportNew');
-    // errors.push(submitErrors);
+    // 3. Insert into SourceStyleImport
+    const submitErrors = await submitAllQueries(data, 'SourceStyleImport');
+    errors.push(submitErrors);
 
     /* 4. Update AndromedaProcessed = 'Yes' where nothing needs to be changed in Andromeda....
           1. The Source Style <> Style and CarryForward = 'No' and OriginalSeasonYear = Season
           2. The Source Style = Style and CarryForward = 'Yes' and OriginalSeasonYear <> Season
     */
 
-    // await submitQuery(`
-    // UPDATE SourceStyleImportNew SET AndromedaProcessed = 'Yes'
-    // WHERE
-    // ((Style = SourceStyle) AND
-    // (CarryForward = 'Yes' and OriginalSeasonYear <> Season))
-    // OR
-    // ((Style <> SourceStyle) AND
-    // (CarryForward = 'No' and Season = OriginalSeasonYear))
-    // `);
+    await submitQuery(`
+    UPDATE SourceStyleImport SET AndromedaProcessed = 'Yes'
+    WHERE
+    ((Style = SourceStyle) AND
+    (CarryForward = 'Yes' and OriginalSeasonYear <> Season))
+    OR
+    ((Style <> SourceStyle) AND
+    (CarryForward = 'No' and Season = OriginalSeasonYear))
+    `);
 
-    // // 5. Get all styles from SourceStyleImport where AndromedaProcessed = 'No' and SourceStyle = Style
-    // const carryForwards = await getSQLServerData(
-    //   'SourceStyleImportNew',
-    //   `WHERE AndromedaProcessed = 'No' and SourceStyle = Style`
-    // );
+    // 5. Get all styles from SourceStyleImport where AndromedaProcessed = 'No' and SourceStyle = Style
+    const carryForwards = await getSQLServerData(
+      'SourceStyleImport',
+      `WHERE AndromedaProcessed = 'No' and SourceStyle = Style`
+    );
 
-    // // 6. Update carry forward flag to true in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
-    // const updateCFErrors = await updateAndromedaData(carryForwards, true);
-    // errors.push(updateCFErrors);
+    // 6. Update carry forward flag to true in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
+    const updateCFErrors = await updateAndromedaData(carryForwards, true);
+    errors.push(updateCFErrors);
 
     // 7. Get all styles from SourceStyleImport where AndromedaProcessed = 'No' and SourceStyle <> Style
     const nonCarryForwards = await getSQLServerData(
-      'SourceStyleImportNew',
+      'SourceStyleImport',
       `WHERE AndromedaProcessed = 'No' and SourceStyle <> Style`
     );
-    console.log(nonCarryForwards);
 
     // 8. Update carry forward flag to true in Andromeda and update AndromedaProcessed = 'Yes' if update was successful
     const updateNonCFErrors = await updateAndromedaData(
